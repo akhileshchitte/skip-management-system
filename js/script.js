691a5c5b8ce642ea0f9215c000ba5dbbe6e0d321
Akhilesh Chitte <42496691+akhileshchitte@users.noreply.github.com>
2021-08-19 00:12:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ closePopupBtn.addEventListener("click", function(){
 });
 
 
-function validate(){
+post function validate(){
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   if(username == "akhilesh" && password == "Pass@123"){
